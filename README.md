d139d3765000c1f84c4faf4a8ea136101023ffe8
Moulin Nathan <nathan.moulin718@gmail.com>
2025-10-10 16:21:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Centipede-Atari2600
+
---
+++ Source/Atari2600Centipede/Atari2600Centipede.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Atari2600Centipede.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Atari2600Centipede, "Atari2600Centipede" );
