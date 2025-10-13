cb56bad0ae399732f765e3fb6555de2adec667dd
Primitation <109825015+Primitation@users.noreply.github.com>
2025-10-13 16:46:00 +0200

    Health | DamageInterface

---
+++ Source/Atari2600Centipede/Private/Health_Component.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Health_Component.h"
+#include "CentipedeLoggerCategories.h"
+
+// Sets default values for this component's properties
+UHealth_Component::UHealth_Component()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealth_Component::BeginPlay()
+{
+	Super::BeginPlay();
+	Health = DefaultHealth;
+}
+
+void UHealth_Component::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	UE_LOG(LogCentipede, Log, TEXT("%s was killed"), *GetOwner()->GetName() );
+}
+
+void UHealth_Component::Damage(float DamageAmount)
+{
+	Health = Health - DamageAmount;
+	if (IsDead()) GetOwner()->Destroy();
+}
---
+++ Source/Atari2600Centipede/Public/Health_Component.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageInterface.h"
+#include "Components/ActorComponent.h"
+#include "Health_Component.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ATARI2600CENTIPEDE_API UHealth_Component : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UHealth_Component();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float DefaultHealth;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Health")
+	float Health;
+
+public:
+
+	void Damage(float DamageAmount);
+	
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	float GetHealth() const { return Health; }
+
+	
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	bool IsDead() const { return Health <= 0.0f; }
+	
+};
